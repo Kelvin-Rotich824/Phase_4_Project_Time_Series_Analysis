d0af235be1c3888abd07719dbee4a8f1cff7f95d
Kelvin Rotich <141912522+Kelvin-Rotich824@users.noreply.github.com>
2024-01-18 17:14:29 +0300

    Editing

--- model_app.py
+++ model_app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
-from prophet import Prophet
+from statsmodels.tsa.arima.model import ARIMA
 import joblib
+import os
+# Load the saved model
+model_directory = "https://github.com/Kelvin-Rotich824/Phase_4_Project_Time_Series_Analysis"
+model_filename = "my_arima_model.pkl"
+model_path = os.path.join(model_directory, model_filename)
+model = joblib.load(model_path)
 
 # Load your time-series data
-df = pd.read_csv("zillow_data.csv")
+df = pd.read_csv("https://github.com/Kelvin-Rotich824/Phase_4_Project_Time_Series_Analysis/zillow_data.csv")
 
 def melt_data(df):
     """
@@ -32,35 +38,26 @@ data = melt_data(df)
 data_diff = data.diff(periods=1).dropna()
 data_diff2 = data_diff.diff(periods=1).dropna()
 
-data_diff2.reset_index(inplace=True)
-data_diff2 = data_diff2.rename(columns={'Date': 'ds', 'value': 'y'})
-
-model = joblib.load('prophet_model.pkl') 
 model.fit(data_diff2)
 
 # Define a function to make predictions
-def predict_future(n_months):
-    future_dates = model.make_future_dataframe(periods=n_months, freq='MS')  # Monthly frequency
-    forecast = model.predict(future_dates)
-    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]  # Include uncertainty
+# Function to make predictions
+def predict_future(n_periods):
+    forecast = model.predict(n_periods=n_periods)
+    return forecast
 
 # Create the Streamlit app
-st.title("Monthly Time Series Forecast App") 
+st.title("ARIMA Forecast App")
+
 # Enable user input for prediction horizon
-n_months = st.number_input("Enter number of months to forecast:", min_value=1, value=12)
+n_periods = st.number_input("Enter number of periods to forecast:", min_value=1)
 
 # Generate predictions based on user input
-forecast_data = predict_future(n_months)
-
-# Display forecast components
-st.subheader("Forecast Components")
-fig = model.plot_components(forecast_data)
-st.write(fig)
+forecast_data = predict_future(n_periods)
 
 # Display forecast data
-st.subheader("Forecast Table")
+st.subheader("Forecast")
 st.write(forecast_data)
 
-# Display interactive line chart
-st.subheader("Interactive Forecast Chart")
+# Visualize the forecast (optional)
 st.line_chart(forecast_data)
